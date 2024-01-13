a051538d1d0ab627308f546c009bb214189178c5
Chenliang Wang <seifer08ms@users.noreply.github.com>
2024-01-14 04:23:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import os
 from openxlab.dataset import get
-import openxlab
-OPENXLAB_AK = os.environ.get('OPENXLAB_AK')
-OPENXLAB_SK = os.environ.get('OPENXLAB_SK')
-openxlab.login(ak=OPENXLAB_AK, sk=OPENXLAB_SK)
 get(dataset_repo='seifer08ms/ass-4bit', target_path='./') 
 os.system('./start.sh')
