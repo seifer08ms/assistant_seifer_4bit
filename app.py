4fc786a93d03bc50812f3553284fd22cf3b850fa
Chenliang Wang <seifer08ms@users.noreply.github.com>
2024-01-16 13:17:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import os
 #OPENXLAB_SK = os.environ.get('OPENXLAB_SK')
 #openxlab.login(ak=OPENXLAB_AK, sk=OPENXLAB_SK)
 #get(dataset_repo='seifer08ms/ass-4bit', target_path='./') 
-git clone https://www.modelscope.cn/seifer08ms/assistant-seifer-4bit.git
+os.system('git clone https://www.modelscope.cn/seifer08ms/assistant-seifer-4bit.git') 
 os.system('./start.sh')
